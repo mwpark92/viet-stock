48d5cd59e662f41a62602341863ae4e7335ec51f
mw92.park <mw92.park@samsung.com>
2020-06-03 17:07:14 +0900

    Add axios setting

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var Crawler = require('crawler');
 var router = express.Router();
+var c = new Crawler({
+  rateLimit: 1000,
+  maxConnections: 10
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/company', function (req, res, next) {
+  c.direct({
+    uri: 'http://www.google.com/',
+    callback: function (error, response) {
+      if (error) {
+        console.log('erro111r');
+        res.send('error')
+      } else {
+        res.send(response)
+      }
+    }
+  });
 });
 
 module.exports = router;
