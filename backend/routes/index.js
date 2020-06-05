a688e26989ae22db569d8a65dd61c199754009e3
mw92.park <mw92.park@samsung.com>
2020-06-05 15:54:41 +0900

    modify url

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -14,8 +14,12 @@ router.get('/', function (req, res, next) {
 router.get('/api/company', function (req, res, next) {
   const ticker = req.query.ticker.toUpperCase();
 
+  //TODO: crawl data using this urls
+  //http://ra.vcsc.com.vn/Financial/PV_Index?ticker=mch&filter=0&unit=1000000000
+  //http://ra.vcsc.com.vn/Financial/PV_CashFlowStatement?ticker=mch&filter=0&unit=1000000000
+
   c.direct({
-    uri: `http://ra.vcsc.com.vn/Company/Financial?lang=en-US&ticker=${ticker}`,
+    uri: `http://ra.vcsc.com.vn/Financial/PV_Index?ticker=${ticker}&filter=0&unit=1000000000`,
     callback: function (error, response) {
       if (error) {
         res.send('error');
